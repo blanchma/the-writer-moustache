27b9b648adb91422b81b8784d4f5f8092bf322f0
blanchma <tute.unique@gmail.com>
2012-10-22 18:09:36 -0300

    Change to Toto extended

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source :rubygems
 
 gem 'builder'
 gem 'rdiscount'
-gem 'toto'
+gem 'toto', git:  'git@github.com:blanchma/toto-extended.git'
 gem 'shotgun'
 
--- public/js/base.js
+++ public/js/base.js
@@ -1,10 +1,33 @@
 
 $(document).ready(function(){
+
+  //Rotate mustaches in header
   $(".header-mustache").mouseover(function(e) {
-    var angle = $(e.target).data('angle') + 90;
+    //var angle = $(e.target).data('angle') + 90;
     $(e.target).rotate({
-      animateTo: angle
+      angle: 0,
+      animateTo: 360//angle
    });
-    $(e.target).data('angle', angle);
+    //$(e.target).data('angle', angle);
+  })
+
+  //Adapt screen to small screens
+  app.adaptToScreen()
+
+  $(window).on('resize', function(){
+    app.adaptToScreen()
   })
 });
+
+var app = {
+  adaptToScreen: function () {
+   console.log($(document).width());
+   if ($(document).width() < 1000) {
+      $(".header-mustache").last().hide();
+      $(".header-mustache").first().css({display: 'block', margin: '30px auto 20px auto'});
+    } else {
+      $(".header-mustache").last().show();
+      $(".header-mustache").first().css({display: 'inline'});
+    }
+  }
+}
