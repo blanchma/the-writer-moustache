309c857a94fc5ab53e2cdcbdcd2232d952496cf3
blanchma <tute.unique@gmail.com>
2013-12-02 17:59:24 -0300

    Post RubyConfArgentina 2013 with Twitter share button

--- public/js/base.js
+++ public/js/base.js
@@ -1,44 +1,27 @@
 
 $(document).ready(function(){
 
-
-  //Adapt screen to small screens
-  app.adaptToScreen()
-
   $(window).on('resize', function(){
-    app.adaptToScreen()
+  })
+
+  $(".divider-mustache").mouseover(function(e) {
+     e.preventDefault();
+     $(e.target).rotate({
+       duration: 2000,
+       angle: 0,
+       animateTo: 360//angle
+     });
+  })
+
+ $(".header-mustache").mouseover(function(e) {
+   e.preventDefault();
+    $(e.target).rotate({
+      duration: 2000,
+      angle: 0,
+      animateTo: 360,
+    });
   })
 });
 
 var app = {
-  adaptToScreen: function () {
-   if ($(document).width() < 1000) {
-      $(".header-mustache").last().hide();
-      $(".header-mustache").first().css({display: 'block', margin: '30px auto 20px auto'});
-
-       //Rotate mustaches in header
-       $(".header-mustache").unbind('mouseover');
-      $(".header-mustache").mouseover(function(e) {
-        $(e.target).rotate({
-          angle: 0,
-          animateTo: 360//angle
-       });
-      })
-
-    } else {
-
-      $(".header-mustache").last().show();
-      $(".header-mustache").first().css({display: 'inline', margin: '0 25px 0 25px'});
-      $(".header-mustache").unbind('mouseover');
-      //Rotate mustaches in header
-      $(".header-mustache").mouseover(function(e) {
-        var angle = $(e.target).data('angle') + 90;
-        $(e.target).rotate({
-          animateTo: angle
-       });
-        $(e.target).data('angle', angle);
-      })
-
-    }
-  }
 }
